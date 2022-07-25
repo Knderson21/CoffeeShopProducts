6edfb06ff105c982d842d2394e39aeab47f93387
Krista <knderson21@gmail.com>
2022-07-25 08:46:29 -0400

    Coffeshop Products Lab

---
+++ CoffeeShopProducts/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using CoffeeShopProducts.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopProducts.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+        private CoffeeShopProductDBContext context = new CoffeeShopProductDBContext();
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> result = context.Products.ToList();
+            return View(result);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.Id == productId);
+            return View(p);
+        }
+    }
+}
